b507f686c9747b0315a75e2b0e22c39b2461b48d
melovarr <meeelnik@gmail.com>
2025-01-27 14:12:44 +0200

    done the first homework

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,13 @@
-function makeTransaction (quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function slugify(title) {
+    // title.;
+    const titleChanged = title.toLowerCase().split(" ");
+    return titleChanged.join("-");
+    // return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 
 
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
