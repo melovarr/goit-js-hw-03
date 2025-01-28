6b381388d4d19a2e7ea51fedf58ec819e6f3efad
melovarr <meeelnik@gmail.com>
2025-01-28 19:22:53 +0200

    chenged condition of for cicle

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     const filterNumbers = [];
-    for (let index = 0; index <= numbers.length; index++) {
+    for (let index = 0; index < numbers.length; index++) {
         if (numbers[index] > value) {
             filterNumbers.push(numbers[index]);
         }
